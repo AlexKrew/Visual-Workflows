b4e02375faa714f26fbdd409860588571b3276b8
Alexander Krewinkel <a-krewinkel@t-online.de>
2022-05-05 16:17:44 +0200

    Added Default Route redirect

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/',
+    redirect: () => {
+      return {path: '/workflow-editor'} 
+    }
+  },
+  {
     path: '/workflow-editor',
     name: 'workflow-editor',
     component: () => import("@/views/WorkflowEditorView")
