fa43cc3cbaaeb068c84284e53acac2f218fb5b13
Alexander Krewinkel <a-krewinkel@t-online.de>
2022-05-08 17:12:50 +0200

    Added Basic Component Structure of WorkflowEditorView

--- frontend/src/views/WorkflowEditorView.vue
+++ frontend/src/views/WorkflowEditorView.vue
@@ -1,10 +1,20 @@
 <template>
-    <p class="text-red-500"> Hello</p>
+  <EditorNodeBar/>
+  <EditorGrid/>
+  <EditorInspector/>
 </template>
 
 <script>
+  import EditorNodeBar from "@/components/workflowEditor/EditorNodeBar.vue";
+  import EditorGrid from "@/components/workflowEditor/EditorGrid.vue";
+  import EditorInspector from "@/components/workflowEditor/EditorInspector.vue";
+  
   export default {
-    
+    components: {
+      EditorNodeBar,
+      EditorGrid,
+      EditorInspector
+    }
   }
 </script>
 
