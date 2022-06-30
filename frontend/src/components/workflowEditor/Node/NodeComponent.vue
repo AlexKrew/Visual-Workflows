a83d571bedf54f636d7a52f29a4b9034d36760c7
Alexander Krewinkel <a-krewinkel@t-online.de>
2022-06-30 12:18:25 +0200

    WIP Added Adding Method

--- frontend/src/components/workflowEditor/Node/NodeComponent.vue
+++ frontend/src/components/workflowEditor/Node/NodeComponent.vue
@@ -1,13 +1,22 @@
 <template>
-  <div :id="nodeModel.id" class="max-w-[200px] absolute w-[200px]" :style="{ left: nodeModel.posGridCell.x + 'px', top: nodeModel.posGridCell.y + 'px' }">
+  <div
+    :id="nodeModel.id"
+    class="max-w-[200px] absolute w-[200px]"
+    :style="{ left: nodeModel.posGridCell.x + 'px', top: nodeModel.posGridCell.y + 'px' }"
+  >
     <Card>
-      <h2 class="text-center">{{nodeModel.title}}</h2>
+      <h2 class="text-center">{{ nodeModel.title }}</h2>
       <div class="w-full">
         <NodePort v-for="port in nodeModel.children" :key="port.id" :port-model="(port as NodePortModel)" />
         <NodePort v-for="port in nodeModel.addedPorts" :key="port.id" :port-model="(port as NodePortModel)" />
       </div>
       <div v-if="nodeModel.addablePorts.length > 0" class="w-full flex justify-center">
-        <button class="w-full p-1 m-3 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded" @click="nodeModel.addAddablePorts()">ADD</button>
+        <button
+          class="w-full p-1 m-3 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded"
+          @click="onAddAddablePorts()"
+        >
+          ADD
+        </button>
       </div>
     </Card>
   </div>
@@ -41,6 +50,14 @@ export default defineComponent({
     function onDragMove(event: InteractEvent) {
       props.nodeModel.addPos(new Vector2(event.dx, event.dy));
     }
+
+    function onAddAddablePorts() {
+      props.nodeModel.addAddablePorts();
+    }
+
+    return {
+      onAddAddablePorts,
+    };
   },
 });
 </script>
