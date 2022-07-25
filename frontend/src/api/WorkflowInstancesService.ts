efc9c74da530ef5bfb99e8f2a86e27af75e9b055
Alexander Krewinkel <a-krewinkel@t-online.de>
2022-07-25 14:58:26 +0200

    Added Api Calls

--- frontend/src/api/WorkflowInstancesService.ts
+++ frontend/src/api/WorkflowInstancesService.ts
@@ -4,45 +4,62 @@ import { HTTP } from "./http/http_common";
 export interface WorkflowInstancesService {
   getWorkflows(): Promise<any>;
   loadWorkflow(id: string): Promise<any>;
+  updateWorkflow(id: string, json: JSON): Promise<any>;
   startWorkflow(id: string): Promise<any>;
   stopWorkflow(id: string): Promise<any>;
   shutdownWorkflow(id: string): Promise<any>;
 }
 
 export class WorkflowInstancesServiceImpl implements WorkflowInstancesService {
-
+  // Fetch all Workflows to show in Index View
   public async getWorkflows(): Promise<WorkflowInfo[]> {
     try {
-      const response = await HTTP.get('/workflows')
-      console.log("GET /workflows response:", response)
+      const response = await HTTP.get("/workflows");
+      console.log("GET /workflows response:", response);
 
-      const workflowInfos: WorkflowInfo[] = []
+      const workflowInfos: WorkflowInfo[] = [];
 
       const data = response.data as unknown[];
-      for(const entry of data) {
-        const info = WorkflowInfo.fromJSON(entry)
-        workflowInfos.push(info)
+      for (const entry of data) {
+        const info = WorkflowInfo.fromJSON(entry);
+        workflowInfos.push(info);
       }
 
-      return workflowInfos
-
-    } catch(err) {
-      console.log("Failed to GET /workflows", err)
-      return []
+      return workflowInfos;
+    } catch (err) {
+      console.log("Failed to GET /workflows", err);
+      return [];
     }
   }
 
+  // Create new empty Workflow
   public async createWorkflow(name: string): Promise<any> {
     try {
-      const response = await HTTP.post('/workflows/new', {name})
-      console.log("Response", response)
-    } catch(err) {
-      return err
+      const response = await HTTP.post("/workflows/new", { name });
+      console.log("Response", response);
+    } catch (err) {
+      return err;
     }
   }
 
+  // Fetch existing Workflow Data
   public async loadWorkflow(id: string): Promise<any> {
-    return null;
+    try {
+      const response = await HTTP.get("/workflows/" + id);
+      console.log("Response", response);
+    } catch (e) {
+      console.log("Failed to GET /workflows/" + id + "\n", e);
+    }
+  }
+
+  // Update existing Workflow
+  public async updateWorkflow(id: string, json: JSON): Promise<any> {
+    try {
+      const response = await HTTP.patch("/workflows/" + id, json);
+      console.log("Response", response);
+    } catch (e) {
+      console.log("Failed to GET /workflows/" + id + "\n", e);
+    }
   }
 
   public async startWorkflow(id: string): Promise<any> {
