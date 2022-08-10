4fca6ed07b756bcae4abdbd542aaec21da240e2e
Alexander Krewinkel <a-krewinkel@t-online.de>
2022-08-10 14:49:19 +0200

    Added Leading Zero at Debug

--- frontend/src/components/util/DateTime.ts
+++ frontend/src/components/util/DateTime.ts
@@ -1,6 +1,15 @@
 class DateTime {
   static getTime(date: Date): string {
-    return `${date.getHours()}:${date.getMinutes()}.${date.getSeconds()}`;
+    const hours = DateTime.getLeadingZero(date.getHours());
+    const minutes = DateTime.getLeadingZero(date.getMinutes());
+    const seconds = DateTime.getLeadingZero(date.getSeconds());
+    return `${hours}:${minutes}.${seconds}`;
+  }
+
+  static getLeadingZero(time: number): string {
+    console.log(time);
+    console.log(("0" + time.toString()).slice(-2));
+    return ("0" + time.toString()).slice(-2);
   }
 }
 
