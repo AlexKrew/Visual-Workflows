a42601ce59c9c486a047c8f8036905f652c6f32f
Alexander Krewinkel <a-krewinkel@t-online.de>
2022-08-28 12:23:54 +0200

    Fixed Gauge Null Bug

--- frontend/src/components/workflowEditor/Dashboard/UIGauge.vue
+++ frontend/src/components/workflowEditor/Dashboard/UIGauge.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
-    <h2 class="font-bold">
-    {{fields.label}}:
-    </h2>
+    <h2 v-if="fields.label" class="font-bold">{{ fields.label }}:</h2>
     <VueGauge
       :options="{
         centralLabel: fields.value.toString(),
         needleValue: getValueInPercent(fields.min_value, fields.max_value, fields.value),
         rangeLabel: [fields.min_value.toString(), fields.max_value.toString()],
-        arcDelimiters: [99.9]
+        arcDelimiters: [99.9],
       }"
       class="min-w"
     ></VueGauge>
@@ -17,8 +15,8 @@
 
 <script lang="ts">
 import DashboardElement from "@/models/Data/Dashboard/DashboardElement";
-import { UIGaugeType } from "@/models/Data/Dashboard/UITypes"
-import { defineComponent, onMounted, ref } from "vue";
+import { UIGaugeType } from "@/models/Data/Dashboard/UITypes";
+import { defineComponent, onBeforeMount, onMounted, ref } from "vue";
 import VueGauge from "vue-gauge";
 
 export default defineComponent({
@@ -35,13 +33,19 @@ export default defineComponent({
   setup(props, ctx) {
     const fields = ref<UIGaugeType>(props.obj.data.fields as UIGaugeType);
 
-    function getValueInPercent(min: number, max: number, value: number):number {
-      return (value -min)/(max-min)*100
+    onBeforeMount(() => {
+      if (!fields.value.min_value) fields.value.min_value = 0;
+      if (!fields.value.max_value) fields.value.max_value = 100;
+      if (!fields.value.value) fields.value.value = fields.value.min_value;
+    });
+
+    function getValueInPercent(min: number, max: number, value: number): number {
+      return ((value - min) / (max - min)) * 100;
     }
 
     return {
       fields,
-      getValueInPercent
+      getValueInPercent,
     };
   },
 });
